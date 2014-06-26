52f3db2726aefd79f0b4a3ade99037bec59d2b95
pe2070597 <pcellsworthus@aol.com>
2014-06-25 20:04:46 -0700

    Signed-off-by: pe2070597 <pcellsworthus@aol.com>

---
+++ Hmwk/CppApplication_4/main.cpp
@@ -0,0 +1,14 @@
+/* 
+ * File:   main.cpp
+ * Author: Paul Ellsworth
+ *
+ * Created on August 9, 2011, 2:48 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {
+      cout<<"Hello World C++"<<endl;
+      return 0;
+}
